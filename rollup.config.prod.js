bbe2e3bb8411b9839716e958c7cfacb2a031715f
joe223 <heyin223@163.com>
2017-07-04 16:46:38 +0800

    add: build config

---
+++ rollup.config.prod.js
@@ -0,0 +1,20 @@
+import typescript from 'rollup-plugin-typescript';
+import uglify from 'rollup-plugin-uglify';
+
+// import alias from 'rollup-plugin-alias';
+// import path from 'path';
+
+export default {
+    entry: 'src/plain.ts',
+    plugins: [
+        typescript({
+            include: 'src/**/*.ts'
+        }),
+        uglify()
+    ],
+    sourceMap: false,
+    moduleName: 'Plain',
+    targets: [
+        { dest: 'dist/plain.js', format: 'umd' }
+    ]
+};
